29f27d01be47b0a1c54bbeec66f76e1b8fb75241
tiaraokta17 <54990376+tiaraoktaviarni06@users.noreply.github.com>
2019-09-14 20:34:50 +0700

    Update 20190801352_kelilinglingkaran.js

--- 20190801352_kelilinglingkaran.js
+++ 20190801352_kelilinglingkaran.js
@@ -1,6 +1,6 @@
 var input_angka = require('readline-sync');
 var phi = 3.14;
 var jari_jari = input_angka.question("masukkan nilai jari jari : ");
-var keliling = phi * phi * jari_jari;
+var keliling = 2 * 3.14 * jari_jari;
 console.log("keliling lingkaran : "+keliling);
 
